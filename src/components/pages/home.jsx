812220cca04cd3c649f40fcec96161d80c250510
Carla Stramandinoli <carla.stram@gmail.com>
2023-06-15 12:29:00 -0300

    Agrego una nueva rama con modificaciones de tabs y del navbar

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import NavBar from '../navBar/navBar'
+import TabsComponents from '../tabsComponents/TabsComponents';
 
  function home() {
   return (
     <div>
         <NavBar />
+        <TabsComponents />
+
         <h1>Aca va la pagina home</h1>
     </div>
   )
---
+++ src/components/tabsComponents/TabsComponents.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Tab from '@mui/material/Tab'
+import Tabs from '@mui/material/Tabs'
+
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+function TabsComponents() {
+    const [selection, setValue] = React.useState("food");
+
+    const handleChange = (_, value) => {
+        setValue(value);
+    }
+    return (
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Box>
+                <Tabs value={selection} onChange={handleChange} aria-label="basic tabs example">
+                    <Tab label="Alimentos" value="food" {...a11yProps(0)} />
+                    <Tab label="Snacks" value="snack" {...a11yProps(1)} />
+                    <Tab label="Contacto" value="contact" {...a11yProps(2)} />
+
+                </Tabs>
+            </Box>
+            <Box>
+                {selection === 'food' ? <Box>Alimentos</Box> : null}
+                {selection === 'snack' ? <Box>Snacks</Box> : null}
+                {selection === 'contact' ? <Box>Contacto</Box> : null}
+
+            </Box>
+        </Box>
+    )
+}
+
+export default TabsComponents;
+
